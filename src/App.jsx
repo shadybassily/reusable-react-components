7a2f105da7f5f16f2339ba558979868a324a1f22
shady bassily <shadybassily@yahoo.com>
2023-07-15 12:04:51 +0300

    created customizable/reusable DatePicker component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,26 @@
+import { useState } from 'react';
+import { AiOutlineCalendar } from 'react-icons/ai';
+
+import DatePicker from './components/inputs/date-picker/DatePicker';
+
 function App() {
-   return <div>
-    
-   </div>;
+   const [date, setDate] = useState(new Date());
+   const handleDateChange = (e) => {
+      setDate(e.target.value);
+   };
+
+   //do something with the date
+
+   return (
+      <div>
+         <DatePicker
+            placeholder="Start date"
+            icon={<AiOutlineCalendar />}
+            onChange={handleDateChange}
+            style={{ fontSize:'15px'}}
+         />
+      </div>
+   );
 }
 
 export default App;
---
+++ src/components/inputs/date-picker/DatePicker.jsx
@@ -0,0 +1,33 @@
+import { useRef } from 'react';
+
+import './datePicker.css';
+
+export default function DatePicker({ icon, style, placeholder = 'Select a date', ...otheProps }) {
+   const dateRef = useRef();
+   const handleToggle = () => {
+      //in order to display the placeholder
+      //the input has to be of type 'text'
+      //then  we change it to 'date'
+      dateRef.current.type = 'date';
+      dateRef.current.showPicker();
+   };
+
+   return (
+      <div className="date-picker-container" style={style}>
+         <input
+            className="date-picker"
+            type="text"
+            ref={dateRef}
+            onFocus={handleToggle}
+            placeholder={placeholder}
+            {...otheProps}
+         />
+
+         {icon && (
+            <div onClick={handleToggle} className="icon">
+               {icon}
+            </div>
+         )}
+      </div>
+   );
+}
