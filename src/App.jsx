8f168ca02cc8b156ac95d7f879024d4633d79bb1
shady bassily <shadybassily@yahoo.com>
2023-07-27 14:20:53 +0300

    hovered label input field

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import DatePicker from './components/inputs/date-picker/DatePicker';
 import BracketsButton from './components/buttons/brackets-button/BracketsButton';
 import Spinner from './components/loaders/spinner/Spinner';
+import Field from './components/inputs/field/Field';
 
 import './App.css';
 
@@ -10,6 +11,10 @@ const components = [
       component: <DatePicker />,
    },
    {
+      title: 'Field',
+      component: <Field />,
+   },
+   {
       title: 'Brackets Button',
       component: <BracketsButton />,
    },
@@ -17,12 +22,13 @@ const components = [
       title: 'Spinner',
       component: <Spinner />,
    },
+  
 ];
 function App() {
    return (
       <div className="App">
          {components.map((c, i) => (
-            <div className="component">
+            <div className="component" key={i}>
                <h2 className="component-title">#{i+1} {c.title}</h2>
                {c.component}
             </div>
--- src/components/inputs/field/Field.jsx
+++ src/components/inputs/field/Field.jsx
@@ -1,5 +1,12 @@
 import './field.css';
 
-export default function Field() {
-   return <div>Field</div>;
+export default function Field({ label = 'Name', name = 'name', ...otherProps }) {
+   return (
+      <div className="input-holder">
+         <input placeholder=" " {...otherProps} />
+         <label className="label" for={name}>
+            {label}
+         </label>
+      </div>
+   );
 }
