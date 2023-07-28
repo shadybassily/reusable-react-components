eeda09ea714c22d110776b28c51a2ecc555e6508
shady bassily <shadybassily@yahoo.com>
2023-07-28 13:57:12 +0300

    deploy to gh-pages

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,9 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+   base: '/reusable-react-components',
    plugins: [react()],
    server: {
       host: true,
    },
-   base: '/reusable-react-components/',
 });
